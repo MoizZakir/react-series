c22ea6dc4a76a20c1bd6bd3492d6a20ce2e8c77f
MoizZakir <moizzakir87@gmail.com>
2024-02-16 22:18:45 +0500

    'eatnsplit_updates'

--- eatnsplit/src/components/splitBill/index.js
+++ eatnsplit/src/components/splitBill/index.js
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import bill.css from '/'
 
-export const SplitBill = ({billUi,userIndex,user}) => {
+export const SplitBill = ({billUi,userIndex,user,setUser}) => {
     let data=[...user]
+    
+    const [myExpense,setMyExpense]=useState()
+    const [freindExpense,setFreindExpense]=useState()
+    const [whoPay,setWhoPay]=useState()
+    // const [whoPay,setWhoPay]=useState()
+    function splitHandler(){
+        console.log(myExpense);
+        console.log(freindExpense);
+        let splitUser=data[userIndex];
+        let count=myExpense-freindExpense
+        if(whoPay!='You'){
+            splitUser.amount=myExpense*1
+        }
+        else{
+            splitUser.amount= -freindExpense*1
+        }
+        setUser(data)
+        console.log(data);
+    }
   return (
-    <div style={{display:'flex',flexDirection:'column',alignItems:'center'}}>
+    <div style={{display:'flex',flexDirection:'column',alignItems:'center' ,border:'1px solid black' }}>
         
         <h4>Split a bill with  {data[userIndex].name}</h4>
-        <div style={{display:'flex' ,flexDirection:'column', gap:"30px" ,marginTop:"10px"}}>
+        <div style={{display:'flex' ,flexDirection:'column', gap:"30px" ,marginTop:"10px", width:"50%"}}>
             <div style={{display:'flex', gap:'20px'}}>
                 <p>BillValue:</p><input type='text' />
 
             </div>
-            <div style={{display:'flex', gap:'20px'}}>
-                <p>Your Expense:</p><input type='text' />
+            <div style={{display:'flex', gap:'20px',  width:"70%"}}>
+                <p>Your Expense:</p><input onChange={(e)=>setMyExpense(e.target.value)} type='number' />
 
             </div>
-            <div style={{display:'flex', gap:'20px'}}>
-                <p>{data[userIndex].name} Expense:</p><input type='text' />
+            <div style={{display:'flex', gap:'20px' ,  width:"70%"}}>
+                <p>{data[userIndex].name} Expense:</p><input onChange={(e)=>setFreindExpense(e.target.value)} type='number' />
 
             </div>
-            <div style={{display:'flex', gap:'20px'}}>
-                <p>Who is paying the Bill:</p><select>
+            <div style={{display:'flex', gap:'20px' ,  width:"90%", }}>
+                <p>Who is paying the Bill:</p><select style={{padding:"5px 15px"}} onChange={(e)=>setWhoPay(e.target.value)}>
                     <option>You</option>
                     <option>{data[userIndex].name}</option>
                 </select>
@@ -28,7 +48,7 @@ export const SplitBill = ({billUi,userIndex,user}) => {
             </div>
             
         </div>
-<button>Split bill</button>
+<button style={{marginTop:"10px", marginLeft:"70px", padding:'5px 15px'}} onClick={()=>splitHandler()}>Split bill</button>
 
     </div>
   )
